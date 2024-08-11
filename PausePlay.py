5b7bf9ce4b0419d9f40b00d69e30ec503bd83257
Tobias S. <58374721+Ashcora@users.noreply.github.com>
2024-08-11 19:50:12 +0200

    Update PausePlay.py

--- PausePlay.py
+++ PausePlay.py
@@ -1,18 +1,25 @@
-# Python script specified for Windows to simulate media play/pause key without e.g. nircmd.exe
-
 import platform
 
 import keyboard
 
 
 def send_media_play_pause():
-    system = platform.system()
-    if system == "Windows":
-        # Simulate media play/pause key press on Windows
-        keyboard.press_and_release("play/pause media")
-    else:
-        print("Unsupported operating system")
+    """
+    Simulates the media play/pause key press on Windows.
+    """
+    try:
+        if platform.system() == "Windows":
+            # Simulate media play/pause key press on Windows
+            keyboard.press_and_release("play/pause media")
+        else:
+            print("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def main():
+    send_media_play_pause()
 
 
 if __name__ == "__main__":
-    send_media_play_pause()
+    main()
